1e7ffb85dbcea9f6f57a73146e3920c4d51889cc
Secondpet <133510333+Secondpet@users.noreply.github.com>
2024-03-06 07:12:33 +0500

    Add files via upload
    Start version 1.0

---
+++ config.py
@@ -0,0 +1,7 @@
+
+TOKEN = '6642579134:AAF1DlTAGvMDgytI7ywTpdQPdcEo3CPJo2M'
+keys = {
+    'рубль' : "RUB",
+    'евро' : "EUR",
+    'доллар' : "USD",
+}
---
+++ extensions.py
@@ -0,0 +1,30 @@
+import json
+
+import requests
+from config import keys
+
+class APIException(Exception):
+    pass
+
+class CashConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты: {base}.')
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту: {quote}.')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту: {base}.')
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество: {amount}. Допускаются только целые числа и числа в десятичной дроби "0.5"')
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+
+        total_base = float(json.loads(r.content)[keys[base]])*float(amount)
+        return total_base
